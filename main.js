eee73dc29e93fa8f7370182750fa5611ba7e67db
Marif-Ismayilov <114796669+Marif-Ismayilov@users.noreply.github.com>
2023-06-17 16:33:27 +0400

    Update main.js

--- main.js
+++ main.js
@@ -12,46 +12,39 @@ enlarge.addEventListener('click', function () {
 decrease.addEventListener('click', function () {
     currentFontSize--;
     lorem.style.fontSize = currentFontSize + "px";
-});
+});                                                 // 17-ci setre kimi fikir verme
 
 let addCity = document.querySelector('.submit')
 let form = document.querySelector('.form');
 let row = document.querySelector('.row');
+let ul = document.querySelector('.ul');             // her ehtimala qarsi 4 eded queryselector
 
 addCity.addEventListener('click', function () {
     
-    let li = document.createElement('li');
-    let text = document.getElementById('fname');
-    let title = text.value;
-    li.innerText = title;
-    li.classList.add('d-flex','w-75','city')
-    
-    if(title != ''){
-        row.appendChild(li);
-    }
-    else{
-        console.log('Seher adi daxil edin');
-        return;
-    }  
-    
-    let icon = document.createElement('button');
-    icon.innerText = 'Delete';
-    icon.classList.add('delete-icon');
-    li.appendChild(icon);
-    
+    let li = document.createElement('li');          
+    let text = document.getElementById('fname');   
+    let title = text.value;                         
+    li.innerText = title;                           
+    li.classList.add('d-flex','w-75','city','list-group-item') 
+    text.value = ('');
+           
 
-    let deleteBtn = document.querySelector('.delete-icon');
-    deleteBtn.addEventListener('click', function(){
-        this.parentNode.remove();
+    let deleteBtn = document.createElement('button');    // her li ucun delete buttonu yaratdim
+    deleteBtn.classList.add('delete-icon');   
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.addEventListener('click', function(){ 
+        this.parentNode.remove();           
+    }); 
+
+    if(!title){      
+        alert('Seher adi daxil edin');
+        return;                
         
-    })
-    if(title != ''){
-        row.append(li);
-        li.append(icon);
-
     }
     else{
-        console.log('Seher adi daxil edin');
-        return;
-    }  
+        ul.append(li);
+        li.append(deleteBtn);
+    }
+    
 });
+
